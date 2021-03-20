d5783058f0f6ee4b26735b988ffa43d53cc5fa62
Janderson Silva <janderson.tic@gmail.com>
2021-03-20 20:29:51 -0300

    perifaCode: plantão javascript 20/03/2021 17h
    reuso de código, funções impuras e efeitos colaterais

--- src/sabado-20210320.js
+++ src/sabado-20210320.js
@@ -9,16 +9,20 @@ const altura = 1.6;
 const peso = 72;
 
 //Processamento
-const imc = peso / (altura ^ 2);
 let resultado;
 
-if (imc <= 18.5) {
-    resultado = "Abaixo do peso";
-} else if (imc <= 24.9) {
-    resultado = "Peso ideal";
-} else {
-    resultado = "Acima do peso";
+function calcularPesoIdeal() {
+    const imc = peso / altura ** 2;
+    if (imc <= 18.5) {
+        resultado = "Abaixo do peso";
+    } else if (imc <= 24.9) {
+        resultado = "Peso ideal";
+    } else {
+        resultado = "Acima do peso";
+    }
 }
+
+calcularPesoIdeal();
 
 //Saida
 console.log(resultado);
